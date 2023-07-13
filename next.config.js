c2ca02828a0167c5dd355adb01a95b5309a9d330
James O'Halloran <james.p.ohalloran@gmail.com>
2023-07-13 11:33:25 -0300

    redirect added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
